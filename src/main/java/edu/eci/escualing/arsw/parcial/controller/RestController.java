a603bcc847574624aae10e8b5d9bc44a1a61237c
migue1994 <migue24co@gmail.com>
2020-03-26 00:04:33 -0500

    Prueba Heroku

--- src/main/java/edu/eci/escualing/arsw/parcial/controller/RestController.java
+++ src/main/java/edu/eci/escualing/arsw/parcial/controller/RestController.java
@@ -1,7 +1,6 @@
 package edu.eci.escualing.arsw.parcial.controller;
 
 import edu.eci.escualing.arsw.parcial.service.AirportService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
--- src/main/java/edu/eci/escualing/arsw/parcial/service/impl/AirportServiceImpl.java
+++ src/main/java/edu/eci/escualing/arsw/parcial/service/impl/AirportServiceImpl.java
@@ -1,13 +1,12 @@
 package edu.eci.escualing.arsw.parcial.service.impl;
 
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
 import edu.eci.escualing.arsw.parcial.cache.CacheService;
 import edu.eci.escualing.arsw.parcial.model.Airport;
 import edu.eci.escualing.arsw.parcial.service.AirportService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.List;
 
 @Service
 public class AirportServiceImpl implements AirportService {
